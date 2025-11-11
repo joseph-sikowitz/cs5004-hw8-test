c06017d23680952f1c176fc4898060ad8070c813
Joe Sikowitz <joseph.sikowitz@gmail.com>
2025-11-11 10:12:07 -0500

    Created the Fixture interface and the ConcreteFixture class.

---
+++ src/model/ConcreteFixture.java
@@ -0,0 +1,50 @@
+package model;
+
+/**
+ * The ConcreteFixture class represents a game fixture that cannot move in a room. It
+ * extends AbstractElement and implements the Fixture, Picturable, and Weightable interfaces.
+ * ConcreteFixtures have a name, description, weight, puzzle, states, and a path to a picture.
+ *
+ * @author Joe Sikowitz
+ * @author Vasilios Nicholas
+ */
+public class ConcreteFixture extends AbstractElement implements Fixture, Picturable, Weightable {
+
+  // attributes
+  private double weight;
+  private Puzzle puzzle;
+  private String states;
+  private String picture;
+
+  /**
+   * The constructor for the Concrete fixture initializes its name and description attributes
+   * using the constructor inherited from AbstractElement. Its other attributes are
+   * initialized here.
+   *
+   * @param name String of the fixture's name.
+   * @param description String of the fixture's description.
+   * @param weight double of the fixture's weight.
+   * @param puzzle Puzzle object that the fixture holds.
+   * @param states String of possible states of the fixture.
+   * @param picture String of the path to the fixture's picture.
+   */
+  public ConcreteFixture(String name, String description, double weight, Puzzle puzzle,
+                         String states,  String picture) {
+    super(name , description);
+
+    this.weight = weight;
+    this.puzzle = puzzle;
+    this.states = states;
+    this.picture = picture;
+  }
+
+  @Override
+  public String getPicturePath() {
+    return this.picture;
+  }
+
+  @Override
+  public double getWeight() {
+    return this.weight;
+  }
+}
---
+++ src/model/Fixture.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * The Fixture interface defines the Fixture type in an adventure game. A fixture
+ * is an element in a Room that does not move. At this time, fixtures have no
+ * required methods.
+ *
+ * @author Joe Sikowitz
+ * @author Vasilios Nicholas
+ */
+public interface Fixture {}
