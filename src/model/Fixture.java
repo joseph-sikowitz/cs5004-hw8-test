70b5f7c921fdde5e55c0488c53ba03109a0bf3e0
Joe Sikowitz <joseph.sikowitz@gmail.com>
2025-11-11 10:45:37 -0500

    Added to Fixture Javadoc.

--- src/model/Fixture.java
+++ src/model/Fixture.java
@@ -3,7 +3,8 @@ package model;
 /**
  * The Fixture interface defines the Fixture type in an adventure game. A fixture
  * is an element in a Room that does not move. At this time, fixtures have no
- * required methods.
+ * required methods. The Fixture interface extends the Weightable and Picturable
+ * interfaces.
  *
  * @author Joe Sikowitz
  * @author Vasilios Nicholas
