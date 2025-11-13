f4327a66e108aebedd1b6a099ac93782df026f1a
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-11-13 16:20:18 -0500

    Implemented custom IllegalArgumentException for attempting to get a Room from a blocked or non-existent passage.

---
+++ src/model/CannotGetRoomException.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ * An IllegalArgumentException for attempting to get a Room from a blocked or non-existent passage.
+ */
+public class CannotGetRoomException extends IllegalArgumentException {
+  private RoomStatus roomExceptionStatus;
+
+  /**
+   * Constructor Initializes the Exception.
+   * @param roomExceptionStatus a RoomStatus enum type representing the
+   *     specific error with the getting the Room.
+   */
+  public CannotGetRoomException(RoomStatus roomExceptionStatus) {
+    super(roomExceptionStatus.getStatus());
+  }
+
+  public RoomStatus getRoomExceptionStatus() {
+    return roomExceptionStatus;
+  }
+}
