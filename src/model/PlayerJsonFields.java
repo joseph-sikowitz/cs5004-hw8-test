e731ce9168c91d3126d044d7a456170ff4757265
Joe Sikowitz <joseph.sikowitz@gmail.com>
2025-11-16 12:50:03 -0500

    Created PlayerJsonFields enum to use when creating a player from a JSON file.

---
+++ src/model/PlayerJsonFields.java
@@ -0,0 +1,36 @@
+package model;
+
+/**
+ * The PlayerJsonFields enum holds the fields that are stored in JSON for players
+ * after saving a game to a file. PlayerJsonFields have a name for their values.
+ */
+public enum PlayerJsonFields {
+  NAME("name"),
+  DESCRIPTION("description"),
+  SCORE("value"),
+  HEALTH("health"),
+  MAX_WEIGHT("max_weight"),
+  INVENTORY("inventory"),
+  ACTIVE_ROOM("active_room");
+
+  private final String name;
+
+  /**
+   * The constructor for the PlayerJsonFields enum sets the name attribute to the
+   * enum's name.
+   *
+   * @param name String of the enum's value.
+   */
+  PlayerJsonFields(String name) {
+    this.name = name;
+  }
+
+  /**
+   * The getValue() method is a getter for the enum's JSON field value.
+   *
+   * @return String of the enum's JSON field value.
+   */
+  String getValue() {
+    return this.name;
+  }
+}
