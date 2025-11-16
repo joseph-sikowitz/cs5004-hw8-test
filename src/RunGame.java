bf1956ae09daf8d118ed5a4eba9ab092f4de37f6
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-11-16 11:43:59 -0500

    Added RunGame class to run GameGameEngineApp with a json file.

---
+++ src/RunGame.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import enginedriver.GameEngineApp;
+
+/**
+ * Class for running GameEngineApp
+ */
+public class RunGame {
+
+  /**
+   * Runs GameEngineApp game with a json data file.
+   * @param args not used.
+   * @throws IOException if file not found.
+   */
+  public static void main(String[] args) throws IOException {
+    GameEngineApp game = new GameEngineApp("data/simple_hallway.json",
+            new InputStreamReader(System.in), System.out);
+    game.start();
+  }
+}
