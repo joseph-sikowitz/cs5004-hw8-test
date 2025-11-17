fbe2933f44bb9aa57d43e3be26a1a922a1418987
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-11-16 21:23:26 -0500

    Edited status for clarity.

--- src/model/TakeItemStatus.java
+++ src/model/TakeItemStatus.java
@@ -7,7 +7,7 @@ public enum TakeItemStatus {
   ITEM_ADDED(" added to your inventory"),
   ITEM_NOT_ADDED_OVER_CAPACITY(" was not added to your inventory."
           + "as you cannot carry anymore weight!"),
-  ITEM_NOT_FOUND(" does not exist.");
+  ITEM_NOT_FOUND(" not found in this Room.");
   private final String status;
 
   /**
