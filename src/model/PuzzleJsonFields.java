3d2bc8f7737b2e7a8d7781b08a1ec5704ac65492
Joe Sikowitz <joseph.sikowitz@gmail.com>
2025-11-17 15:49:41 -0500

    Added Javadoc to PuzzleJsonFields.

--- src/model/PuzzleJsonFields.java
+++ src/model/PuzzleJsonFields.java
@@ -1,5 +1,12 @@
 package model;
 
+/**
+ * The PuzzleJsonFields enum represents the fields that will appear in an incoming JSON
+ * game file. Each value is mapped to a field name.
+ *
+ * @author Joe Sikowitz
+ * @author Vasilios Nicholas
+ */
 public enum PuzzleJsonFields {
   NAME("name"),
   ACTIVE("active"),
@@ -14,10 +21,20 @@ public enum PuzzleJsonFields {
 
   private final String value;
 
+  /**
+   * The constructor for the PuzzleJsonFields enum initializes the value's field name.
+   *
+   * @param value String of the field name.
+   */
   PuzzleJsonFields(String value) {
     this.value = value;
   }
 
+  /**
+   * The getter for the enum's field name.
+   *
+   * @return String of the field name.
+   */
   String getValue() {
     return this.value;
   }
