42b10b4bca6fb88d5cb8ba328e3f37ffd25d155a
Joe Sikowitz <joseph.sikowitz@gmail.com>
2025-11-18 15:35:03 -0500

    Added a trivial test class for GameController.

---
+++ test/controller/GameControllerTest.java
@@ -0,0 +1,31 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+import java.io.InputStreamReader;
+
+import model.AdventureGameModel;
+import model.IAdventureGameModel;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The GameControllerTest performs a trivial test of the GameController class
+ * by checking that the constructor instantiates a GameController object. Since
+ * GameController's behavior is driven by user input, the go() method is not
+ * tested.
+ */
+class GameControllerTest {
+
+  /**
+   * The testConstructor() method tests the GameController constructor to see
+   * that it instantiates a GameController object.
+   */
+  @Test
+  void testConstructor() {
+    String gameFileName = "data/simple_hallway.json";
+    IAdventureGameModel model = new AdventureGameModel(gameFileName);
+    GameController controller = new GameController(
+            new InputStreamReader(System.in), System.out, model);
+    assertInstanceOf(GameController.class, controller);
+  }
+}
