b66f84260968e55b9f177b9c1b1241baab729254
Joe Sikowitz <joseph.sikowitz@gmail.com>
2025-11-23 14:13:18 -0500

    Created ICommand interface for command pattern in controller.

---
+++ src/controller/ICommand.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * ICommand is the interface for controller commands in the adventure game.
+ */
+public interface ICommand {
+
+  /**
+   * The execute() method executes the command defined by the class.
+   */
+  void execute();
+}
