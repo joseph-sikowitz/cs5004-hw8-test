82118ff02242901986416919cd546c58b298d97a
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-11-27 22:17:20 -0500

    Removed test case for startGame method

--- test/controller/GameCommandReaderTest.java
+++ test/controller/GameCommandReaderTest.java
@@ -9,38 +9,20 @@ import java.io.StringReader;
 import org.junit.jupiter.api.Test;
 
 /**
- * The GameCommandReaderTest performs trivial tests of the GameCommandReader class
+ * The GameCommandReaderTest performs trivial tests of the GameTextInputOutputProcessor class
  * since its functionality is based on user input.
  */
 class GameCommandReaderTest {
 
   /**
-   * The testConstructor() method tests the GameCommandReader's constructor by
-   * asserting that a new GameCommandReader object is created.
+   * The testConstructor() method tests the GameTextInputOutputProcessor's constructor by
+   * asserting that a new GameTextInputOutputProcessor object is created.
    */
   @Test
   void testConstructor() {
     BufferedReader stringReader = new BufferedReader(new StringReader("Joe"));
-    GameCommandReader gcr = new GameCommandReader(stringReader, System.out);
-    assertInstanceOf(GameCommandReader.class, gcr);
-  }
-
-  /**
-   * The testStartGame() method tests startGame() by checking that the input
-   * provided is the result of starting the game.
-   */
-  @Test
-  void testStartGame() {
-    BufferedReader stringReader = new BufferedReader(new StringReader("Joe"));
-    GameCommandReader gcr = new GameCommandReader(stringReader, System.out);
-
-    String result = "";
-    try {
-      result = gcr.startGame();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    assertEquals("Joe", result);
+    GameTextInputOutputProcessor gcr = new GameTextInputOutputProcessor(stringReader, System.out);
+    assertInstanceOf(GameTextInputOutputProcessor.class, gcr);
   }
 
 }
