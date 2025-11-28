e434c094350989b1831fa606b1803c5bc67c664c
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-11-28 09:51:27 -0500

    Fixed call to this.model.getPlayerName() prior to starting game.

--- src/controller/StartGameCommand.java
+++ src/controller/StartGameCommand.java
@@ -18,8 +18,8 @@ public class StartGameCommand extends AbstractCommand {
   public void execute() throws IOException {
     processor.messageToPlayer(UserPrompts.NEW_PLAYER_PROMPT.getPrompt());
     this.model.setPlayerName(processor.getUserMessage());
+    this.model.loadGameData();
     processor.messageToPlayer(UserPrompts.NEW_PLAYER_NAME_PROMPT.getPrompt()
             + this.model.getPlayerName()  + "\n");
-    this.model.loadGameData();
   }
 }
