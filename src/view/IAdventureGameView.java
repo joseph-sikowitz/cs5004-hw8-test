75bade66a28cf3983421603e598fbb91c988ff15
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-11-27 22:16:18 -0500

    Refactored to take in a subtype of GameInputOutProcessor.

--- src/view/IAdventureGameView.java
+++ src/view/IAdventureGameView.java
@@ -2,16 +2,16 @@ package view;
 
 
 
-import controller.GameCommandReader;
+import java.io.IOException;
 
-//TODO: Create Supertype for GameCommandReader called CommandInterpreter.
-//TODO: make generic C able to be any subtype of CommandInterpreter.
+import controller.GameTextInputOutputProcessor;
+
 /**
  * An Interface the view subsystem of an AdventureGame.
  * @param <T> The type of data to be passed to the view.
  * @param <C> The type of CommandInterpreter that the view will pass data back to.
  */
-public interface IAdventureGameView<T, C extends GameCommandReader> {
+public interface IAdventureGameView<T, C extends GameTextInputOutputProcessor> {
 
   /**
    * Sets the controller class that interprets commands passed in through the view.
@@ -28,37 +28,43 @@ public interface IAdventureGameView<T, C extends GameCommandReader> {
   /**
    * Displays message to player.
    * @param data the data to display.
+   * @throws IOException If I/O error occurs.
    */
-  void messageToPlayer(T data);
+  void messageToPlayer(T data) throws IOException;
 
   /**
    * Updates the Player's stats.
    * @param data the data to display.
+   * @throws IOException If I/O error occurs.
    */
-  void updatePlayerStats(T data);
+  void updatePlayerStats(T data) throws IOException;
 
   /**
    * Updates the Room's name and description.
    * @param data the data to display.
+   * @throws IOException If I/O error occurs.
    */
-  void updateRoom(T data);
+  void updateRoom(T data) throws IOException;
 
   /**
    * Updates the ELements that the Player can examine.
    * @param data the data to display.
+   * @throws IOException If I/O error occurs.
    */
-  void updateExaminer(T data);
+  void updateExaminer(T data) throws IOException;
 
   /**
    * Updates the Player's inventory.
    * @param data the data to display.
+   * @throws IOException If I/O error occurs.
    */
-  void updateInventory(T data);
+  void updateInventory(T data) throws IOException;
 
   /**
    * Updates the player Affector.
    * @param data the data to display.
+   * @throws IOException If I/O error occurs.
    */
-  void updatePlayerAffector(T data);
+  void updatePlayerAffector(T data) throws IOException;
 
 }
