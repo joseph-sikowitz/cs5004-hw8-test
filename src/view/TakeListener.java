ed498d08a4dc6db3e26ab00470df8676797b3da0
Joe Sikowitz <joseph.sikowitz@gmail.com>
2025-12-02 16:16:30 -0500

    Added result attribute to TakeListener and get and remove methods for it.

--- src/view/TakeListener.java
+++ src/view/TakeListener.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 public class TakeListener implements ActionListener {
 
   private final List<String> items;
+  private String result;
 
   public TakeListener(List<String> items) {
     this.items = items;
@@ -22,8 +23,16 @@ public class TakeListener implements ActionListener {
       list.setListData(strList);
       JScrollPane scrollPane = new JScrollPane(list);
       JOptionPane.showMessageDialog(null, scrollPane, "Take", JOptionPane.PLAIN_MESSAGE);
-      list.getSelectedValue();
+      this.result = "take " + list.getSelectedValue();
     }
+  }
+
+  public String getResult() {
+    return this.result;
+  }
+
+  public void removeResult() {
+    this.result = null;
   }
 
 }
