feff6bf21872d1796a6bd24e91fc86a0a3913ed1
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-12-05 18:53:38 -0500

    Changed where message List is declared.

--- src/controller/EndOfTurnActionsCommand.java
+++ src/controller/EndOfTurnActionsCommand.java
@@ -24,13 +24,14 @@ public class EndOfTurnActionsCommand extends AbstractCommand {
 
   @Override
   public boolean execute() throws IOException {
-    List<String> message = new ArrayList<>();
+
     //if there is a Monster in the room, have it "affect" the Player in the model.
     //message.add(this.model.affectPlayer());
     this.processor.messageToPlayer(this.model.affectPlayer());
 
     if (this.model.changeInPlayerHealthStatus() | this.model.changeInPlayerScore()
             | this.model.changeInPlayerRank()) {
+      List<String> message = new ArrayList<>();
       //display player's health status if it has changed since last command.
       message.add(this.model.getPlayerHealthStatus());
       //display player's score if it has changed since last command.
