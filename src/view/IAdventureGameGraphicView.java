a874ba4148ffadee0bb86fbad2f78eba007d7f5d
Vasilios Nicholas <vasiliosnicholasgr@gmail.com>
2025-12-05 18:26:00 -0500

    Javadoc

--- src/view/IAdventureGameGraphicView.java
+++ src/view/IAdventureGameGraphicView.java
@@ -3,15 +3,15 @@ package view;
 import java.awt.event.ActionListener;
 
 /**
- * Represents a Graphics view for the adventure game that
- * @param <T>
+ * Represents a Graphics IAdventureGameView that uses an Event loop.
+ * @param <T> a class Generic representing the type of Data to receive from the Controller.
  */
 public interface IAdventureGameGraphicView<T> extends IAdventureGameView<T> {
 
 
   /**
    * Sets the event handler for the Graphics view.
-   * @param actionListener
+   * @param actionListener an ActionListener type that reacts to ActionEvents.
    */
   void setEventHandler(ActionListener actionListener);
 
